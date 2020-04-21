3494d8c8db6725af2bacb0f6a06637eadfc77265
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2020-04-21 12:05:50 +0600

    adding menu

--- src/javanotepad/JavaNotePad.java
+++ src/javanotepad/JavaNotePad.java
@@ -2,6 +2,9 @@ package javanotepad;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -12,6 +15,9 @@ import javax.swing.JTextArea;
 public class JavaNotePad extends JFrame {
 
     JTextArea mainarea;
+    JMenuBar mbar;
+    JMenu mnuFile, mnuEdit, mnuFormat, mnuHelp;
+    JMenuItem itmNew, itmOpen, itmSave;
 
     public JavaNotePad() {
         initConponent();
@@ -20,8 +26,33 @@ public class JavaNotePad extends JFrame {
     private void initConponent() {
         mainarea = new JTextArea();
         getContentPane().add(mainarea);
+        //Scroll bar adding for more text
         getContentPane().add(new JScrollPane(mainarea),
                 BorderLayout.CENTER);
+        
+        //menu bar
+        mbar = new JMenuBar();
+        //menu
+        mnuEdit = new JMenu("Edit");
+        mnuFile = new JMenu("File");
+        mnuFormat = new JMenu("Fromat");
+        mnuHelp = new JMenu("Help");
+        //menu Item
+        itmNew = new JMenuItem("New");
+        itmOpen = new JMenuItem("Open");
+        itmSave = new JMenuItem("Save");
+       //Add menu item
+        mnuFile.add(itmNew);
+        mnuFile.add(itmOpen);
+        mnuFile.add(itmSave);
+        //Add menu item to menu Bar
+        mbar.add(mnuFile);
+        mbar.add(mnuEdit);
+        mbar.add(mnuFormat);
+        mbar.add(mnuHelp);
+        //add menubar to frame
+        setJMenuBar(mbar);
+        
         setTitle("Untitled nodepad");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
